ae7aec471f2970431220fbb9353d1d9b66477e5d
dianaambar <dianaambar745@gmail.com>
2019-05-13 00:19:55 +0700

    nyambungin ke adapter

--- app/src/main/java/pnj/ac/id/foodless2/FoodfactFragment.java
+++ app/src/main/java/pnj/ac/id/foodless2/FoodfactFragment.java
@@ -4,28 +4,30 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link FoodfactFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link FoodfactFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import pnj.ac.id.foodless2.Model.Berita;
+
+
+///**
+// * A simple {@link Fragment} subclass.
+// * Activities that contain this fragment must implement the
+// * {@link FoodfactFragment.OnFragmentInteractionListener} interface
+// * to handle interaction events.
+// * Use the {@link FoodfactFragment#newInstance} factory method to
+// * create an instance of this fragment.
+// */
 public class FoodfactFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String Tag = "FoodfactFragment";
+    private ArrayList<Berita> beritaList =new ArrayList<>();
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -33,31 +35,27 @@ public class FoodfactFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FoodfactFragment.
-     */
+//    /**
+//     * Use this factory method to create a new instance of
+//     * this fragment using the provided parameters.
+//     *
+//     * @param param1 Parameter 1.
+//     * @param  Parameter 2.
+//     * @return A new instance of fragment FoodfactFragment.
+//     */
     // TODO: Rename and change types and number of parameters
-    public static FoodfactFragment newInstance(String param1, String param2) {
-        FoodfactFragment fragment = new FoodfactFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+//    public static FoodfactFragment newInstance(String param1, String param2) {
+//        FoodfactFragment fragment = new FoodfactFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -66,6 +64,7 @@ public class FoodfactFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_foodfact, container, false);
     }
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
---
+++ app/src/main/java/pnj/ac/id/foodless2/Model/Berita.java
@@ -0,0 +1,40 @@
+package pnj.ac.id.foodless2.Model;
+
+public class Berita {
+    private String judulBerita;
+    private String isiBerita;
+    private String gambarBerita;
+
+    public Berita(String judulBerita, String isiBerita, String gambarBerita){
+        this.judulBerita = judulBerita;
+        this.isiBerita = isiBerita;
+        this.gambarBerita = gambarBerita;
+    }
+
+    public Berita(){}
+
+    public String getJudulBerita() {
+        return judulBerita;
+    }
+
+    public void setJudulBerita(String judulBerita) {
+        this.judulBerita = judulBerita;
+    }
+
+    public String getIsiBerita() {
+        return isiBerita;
+    }
+
+    public void setIsiBerita(String isiBerita) {
+        this.isiBerita = isiBerita;
+    }
+
+    public String getGambarBerita() {
+        return gambarBerita;
+    }
+
+    public void setGambarBerita(String gambarBerita) {
+        this.gambarBerita = gambarBerita;
+    }
+
+}
