e3431815f1f4b7de45c566861e0a1199e588146a
Intan Yoshana <intan318@gmail.com>
2019-06-25 14:20:27 +0700

    profile changed

--- app/src/main/java/pnj/ac/id/foodless/Fragment/ProfileFragment.java
+++ app/src/main/java/pnj/ac/id/foodless/Fragment/ProfileFragment.java
@@ -1,8 +1,5 @@
 package pnj.ac.id.foodless.Fragment;
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.Intent;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -13,12 +10,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -27,9 +18,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import org.w3c.dom.Text;
 
-import pnj.ac.id.foodless.Activity.Login;
 import java.util.Objects;
 
 import pnj.ac.id.foodless.Activity.Form;
@@ -44,26 +33,6 @@ import static android.support.constraint.Constraints.TAG;
 
 public class ProfileFragment extends Fragment {
 
-    private static final String TAG = "ProfileFragment";
-
-    private static final int ACTIVITY_NUM = 4;
-
-    private View ProfileView;
-
-    private Context mContext;
-
-    private ProgressBar mProgressBar;
-
-    //widgets
-    private TextView tFullName, tEmailUser, tPasswordUser, tPhoneUser, tAddressUser;
-    private Button btnSignOutProfile;
-
-    //firebase
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-    private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference myRef;
-
     private TextView fullname, emailUser, addressUser, no_phoneUser, logout;
     private View ProfileView;
 
@@ -71,73 +40,7 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-       View view = inflater.inflate(R.layout.fragment_profile, container, false);
-
-       tFullName = view.findViewById(R.id.txtFullName);
-       tEmailUser = view.findViewById(R.id.txtEmailUser);
-       tPasswordUser = view.findViewById(R.id.txtPasswordUser);
-       tPhoneUser = view.findViewById(R.id.txtPhoneUser);
-       tAddressUser = view.findViewById(R.id.txtAddressUser);
-
-       mProgressBar = (ProgressBar) view.findViewById(R.id.profileProgressBar);
-
-       mContext = getActivity();
-
-       btnSignOutProfile = (Button) view.findViewById(R.id.btnSignOutUser);
-       Log.d(TAG, "onCreateView: started.");
-
-       return view;
-    }
-
-    /*
-        Setup the firebase auth object
-     */
-
-    private void setupFirebaseAuth(){
-        Log.d(TAG, "setupFirebaseAuth: setting up firebase auth.");
-
-        mAuth = FirebaseAuth.getInstance();
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        myRef = mFirebaseDatabase.getInstance().getReference().child("Users").child("1");
-
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-
-                if (user != null){
-                    //User is signed in
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                } else {
-                    //User is signed out
-                    Log.d(TAG, "onAuthStateChanged:signed_out");
-                }
-            }
-        };
-
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                //retrieve user information from the database
-                String full_name = dataSnapshot.child("fullName").getValue().toString();
-                String email = dataSnapshot.child("email").getValue().toString();
-                String address = dataSnapshot.child("address").getValue().toString();
-                String password = dataSnapshot.child("password").getValue().toString();
-                String phone = dataSnapshot.child("phone").getValue().toString();
-
-                tFullName.setText(full_name);
-                tEmailUser.setText(email);
-                tPasswordUser.setText(password);
-                tAddressUser.setText(address);
-                tPhoneUser.setText(phone);
-                //retrieve images for the user in question
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                ProfileView = inflater.inflate(R.layout.fragment_profile, container, false);
+        ProfileView = inflater.inflate(R.layout.fragment_profile, container, false);
 
         return ProfileView;
 
@@ -168,36 +71,12 @@ public class ProfileFragment extends Fragment {
         });
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        if (mAuth.getCurrentUser() == null){
-            finish();
-            startActivity(new Intent(this, Login.class));
-
-        }    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
-    }
-
-//    private void checkCurrentUser(FirebaseUser user){
-//        Log.d(TAG, "checkCurrentUser: checking if user is logged in.");
-//
-//        if(user == null){
-//            Intent intent = new Intent (mCon)
-//        }
-//    }
     private void getUserDetail() {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference retrieveUser = database.getReference("Users");
+        DatabaseReference retriveUser = database.getReference("Users");
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            retrieveUser.child(Objects.requireNonNull(FirebaseAuth.getInstance()).getCurrentUser().getUid())
+            retriveUser.child(Objects.requireNonNull(FirebaseAuth.getInstance()).getCurrentUser().getUid())
                     .addValueEventListener(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -221,4 +100,7 @@ public class ProfileFragment extends Fragment {
                     });
         }
     }
+
+
+
 }
