ce351ae678757b48b5716f21d21605b7009e819b
Intan Yoshana <intan318@gmail.com>
2019-06-25 14:29:03 +0700

    user

--- app/src/main/java/pnj/ac/id/foodless/Model/User.java
+++ app/src/main/java/pnj/ac/id/foodless/Model/User.java
@@ -2,13 +2,15 @@ package pnj.ac.id.foodless.Model;
 
 public class User {
 
-    private User user;
-
     private String full_name;
     private String address;
     private String phone;
     private String email;
     private String password;
+
+    public User(){
+
+    }
 
     public User (String full_name, String address, String phone, String email, String password){
         this.full_name = full_name;
@@ -18,23 +20,11 @@ public class User {
         this.password = password;
     }
 
-    public User(){
-
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getFull_name() {
+    public String getFullName(){
         return full_name;
     }
 
-    public void setFull_name(String full_name) {
+    public void setFullName(String full_name){
         this.full_name = full_name;
     }
 
@@ -42,79 +32,31 @@ public class User {
         return address;
     }
 
-    public void setAddress(String address) {
+    public void setAddress(){
         this.address = address;
     }
 
-    public String getPhone() {
+    public String getPhone(){
         return phone;
     }
 
-    public void setPhone(String phone) {
+    public void setPhone(){
         this.phone = phone;
     }
 
-    public String getEmail() {
+    public String getEmail(){
         return email;
     }
 
-    public void setEmail(String email) {
+    public void setEmail(){
         this.email = email;
     }
 
-    public String getPassword() {
+    public String getPassword(){
         return password;
     }
 
-    public void setPassword(String password) {
+    public void setPassword(){
         this.password = password;
-    }
-
-    //    public String getFullName(){
-//        return full_name;
-//    }
-//
-//    public void setFullName(String full_name){
-//        this.full_name = full_name;
-//    }
-//
-//    public String getAddress() {
-//        return address;
-//    }
-//
-//    public void setAddress(){
-//        this.address = address;
-//    }
-//
-//    public String getPhone(){
-//        return phone;
-//    }
-//
-//    public void setPhone(){
-//        this.phone = phone;
-//    }
-//
-//    public String getEmail(){
-//        return email;
-//    }
-//
-//    public void setEmail(){
-//        this.email = email;
-//    }
-//
-//    public String getPassword(){
-//        return password;
-//    }
-//
-//    public void setPassword(){
-//        this.password = password;
-//    }
-
-
-    @Override
-    public String toString() {
-        return "User{" +
-        "full_name='" + full_name + '\'' + ", address='" + address + '\'' +
-        ", phone='" + phone + '\'' + ", email='" + email + '\'' + ", password= '" + password + '\'' + '}';
     }
 }
