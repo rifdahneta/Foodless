03e3a17c2d5c4a95baedd8fb144acdb86ec8d9ea
Intan Yoshana <intan318@gmail.com>
2019-06-27 20:07:26 +0700

    multi level login
    login for two roles (donatur dan komunitas)

--- app/src/main/java/pnj/ac/id/foodless/Activity/Login.java
+++ app/src/main/java/pnj/ac/id/foodless/Activity/Login.java
@@ -5,9 +5,12 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -15,6 +18,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import pnj.ac.id.foodless.R;
 
@@ -23,12 +31,25 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     private EditText editLogEmail, editLogPassword;
     private Button btnLogin, btnLogRegis;
     private ProgressBar progressBar;
+    private Spinner spinnerRole;
+
     private FirebaseAuth auth;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+//            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.roleUser, android.R.layout.simple_spinner_item);
+//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+//        spinnerRole.setAdapter(adapter);
+//        spinnerRole.setOnItemSelectedListener(this);
+
         init();
     }
 
@@ -40,29 +61,30 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
         progressBar = findViewById(R.id.progress_login);
 
+        spinnerRole = findViewById(R.id.spinnerRole);
         btnLogin.setOnClickListener(this);
         btnLogRegis.setOnClickListener(this);
 
         auth = FirebaseAuth.getInstance();
 
-        if(auth.getCurrentUser() != null) {
+        if (auth.getCurrentUser() != null) {
             startActivity(new Intent(Login.this, MainActivity.class));
         }
     }
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.button_login){
+        if (v.getId() == R.id.button_login) {
             String email = editLogEmail.getText().toString().trim();
             final String password = editLogPassword.getText().toString().trim();
 
-            if(email.isEmpty()){
+            if (email.isEmpty()) {
                 editLogEmail.setError(getString(R.string.error_input_email));
                 editLogEmail.requestFocus();
                 return;
             }
 
-            if(password.isEmpty()){
+            if (password.isEmpty()) {
                 editLogPassword.setError(getString(R.string.error_input_password));
                 editLogPassword.requestFocus();
                 return;
@@ -74,8 +96,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     progressBar.setVisibility(View.GONE);
-                    if (!task.isSuccessful())
-                    {
+                    if (!task.isSuccessful()) {
                         Toast.makeText(Login.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
                     } else {
                         checkIfEmailVerified();
@@ -84,35 +105,53 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
             });
         }
 
-        if (v.getId()== R.id.buttonlog_register)
-        {
-            Intent i = new Intent (Login.this, ChooseRegister.class);
+        if (v.getId() == R.id.buttonlog_register) {
+            Intent i = new Intent(Login.this, ChooseRegister.class);
             startActivity(i);
-
         }
-
-
-
     }
 
     private void checkIfEmailVerified() {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        assert user != null;
-        if (user.isEmailVerified()){
-            //email verified, proceed to finish this activity
-            finish();
-            Toast.makeText(Login.this, "Successfully logged in", Toast.LENGTH_LONG).show();
 
-            Intent i = new Intent(Login.this, MainActivity.class);
-            startActivity(i);
-            finish();
-        } else if (!user.isEmailVerified()) {
-            Toast.makeText(Login.this, "Email is not verified", Toast.LENGTH_LONG).show();
-            FirebaseAuth.getInstance().signOut();
-        } else {
-            Toast.makeText(Login.this, "An error occured. Please try again", Toast.LENGTH_SHORT).show();
+        if (spinnerRole.getSelectedItem().toString().equalsIgnoreCase("Donatur")) {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            assert user != null;
+            if (user.isEmailVerified()) {
+                //email verified, proceed to finish this activity
+                finish();
+                Toast.makeText(Login.this, "Successfully logged in", Toast.LENGTH_LONG).show();
 
+                Intent i = new Intent(Login.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+            } else if (!user.isEmailVerified()) {
+                Toast.makeText(Login.this, "Email is not verified", Toast.LENGTH_LONG).show();
+                FirebaseAuth.getInstance().signOut();
+            } else {
+                Toast.makeText(Login.this, "An error occured. Please try again", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            if (spinnerRole.getSelectedItem().toString().equalsIgnoreCase("Komunitas")) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                assert user != null;
+                if (user.isEmailVerified()) {
+                    //email verified, proceed to finish this activity
+                    finish();
+                    Toast.makeText(Login.this, "Successfully logged in", Toast.LENGTH_LONG).show();
+
+                    Intent i = new Intent(Login.this, MainActivityKomunitas.class);
+                    startActivity(i);
+
+                    finish();
+                } else if (!user.isEmailVerified()) {
+                    Toast.makeText(Login.this, "Email is not verified", Toast.LENGTH_LONG).show();
+                    FirebaseAuth.getInstance().signOut();
+                } else {
+                    Toast.makeText(Login.this, "An error occured. Please try again", Toast.LENGTH_SHORT).show();
+                }
+            }
         }
+    }
 
-        }
 }
--- app/src/main/java/pnj/ac/id/foodless/Model/Communities.java
+++ app/src/main/java/pnj/ac/id/foodless/Model/Communities.java
@@ -9,65 +9,43 @@ public class Communities {
     private String alamat_komunitas;
     private String email_komunitas;
     private String password_komunitas;
-    private String jmlmember_komunitas;
+    private String jumlah_member;
 
-    public Communities(){
 
-    }
-
-    public Communities(String namaKomunitas, String alamatKomunitas, String notelpKomunitas, String emailKomunitas, String passKomunitas, String jmlMemberKomunitas){
-        this.nama_komunitas = nama_komunitas;
-        this.jenis_kegiatan = jenis_kegiatan;
-        this.gambar_komunitas = gambar_komunitas;
-        this.notelp_komunitas = notelp_komunitas;
-        this.alamat_komunitas = alamat_komunitas;
-        this.email_komunitas = email_komunitas;
-        this.password_komunitas = password_komunitas;
-        this.jmlmember_komunitas = jmlmember_komunitas;
-    }
 
     public String getNama_komunitas() {
-
         return nama_komunitas;
     }
 
     public void setNama_komunitas(String nama_komunitas) {
-
         this.nama_komunitas = nama_komunitas;
     }
 
-    public String getJenis_kegiatan()
-    {
+    public String getJenis_kegiatan() {
         return jenis_kegiatan;
     }
 
-    public void setJenis_kegiatan(String jenis_kegiatan)
-    {
+    public void setJenis_kegiatan(String jenis_kegiatan) {
         this.jenis_kegiatan = jenis_kegiatan;
     }
 
     public String getGambar_komunitas() {
-
         return gambar_komunitas;
     }
 
     public void setGambar_komunitas(String gambar_komunitas) {
-
         this.gambar_komunitas = gambar_komunitas;
     }
 
     public String getnotelp_komunitas() {
-
         return notelp_komunitas;
     }
 
     public void setnotelp_komunitas(String notelp_komunitas) {
-
         this.notelp_komunitas = notelp_komunitas;
     }
 
     public String getAlamat_komunitas() {
-
         return alamat_komunitas;
     }
 
@@ -76,7 +54,6 @@ public class Communities {
     }
 
     public String getEmail_komunitas() {
-
         return email_komunitas;
     }
 
@@ -92,12 +69,38 @@ public class Communities {
         this.password_komunitas = password_komunitas;
     }
 
-    public String getJmlmember_komunitas() {
-        return jmlmember_komunitas;
+    public String getjumlah_member() {
+        return jumlah_member;
     }
 
-    public void setJmlmember_komunitas(String jmlmember_komunitas) {
-        this.jmlmember_komunitas = jmlmember_komunitas;
+    public void setjumlah_member(String jumlah_member) {
+        this.jumlah_member = jumlah_member;
+    }
+
+    public Communities(String namaKomunitas, String alamatKomunitas, String notelpKomunitas, String emailKomunitas, String passKomunitas, String jumlah_memberKomunitas){
+        this.nama_komunitas = namaKomunitas;
+        this.jenis_kegiatan = jenis_kegiatan;
+        this.gambar_komunitas = gambar_komunitas;
+        this.notelp_komunitas = notelpKomunitas;
+        this.alamat_komunitas = alamatKomunitas;
+        this.email_komunitas = emailKomunitas;
+        this.password_komunitas = passKomunitas;
+        this.jumlah_member = jumlah_memberKomunitas;
+    }
+
+    public Communities(){
+
+    }
+
+    public Communities(String nama_komunitas, String jenis_kegiatan, String gambar_komunitas, String notelp_komunitas, String alamat_komunitas, String email_komunitas, String password_komunitas, String jumlah_member) {
+        this.nama_komunitas = nama_komunitas;
+        this.jenis_kegiatan = jenis_kegiatan;
+        this.gambar_komunitas = gambar_komunitas;
+        this.notelp_komunitas = notelp_komunitas;
+        this.alamat_komunitas = alamat_komunitas;
+        this.email_komunitas = email_komunitas;
+        this.password_komunitas = password_komunitas;
+        this.jumlah_member = jumlah_member;
     }
 
 }
